db68eb44f0eba14b5086550208f210859944828e
veeresh-27 <veereshrm789@gmail.com>
2022-09-14 13:07:07 +0530

    cd

--- src/constants.js
+++ src/constants.js
@@ -16,10 +16,7 @@ export const techStack = [
   { name: "Tailwind CSS", link: "https://www.vectorlogo.zone/logos/tailwindcss/tailwindcss-icon.svg" },
   { name: "Heroku", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-plain.svg" },
   { name: "Git", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-plain.svg" },
-  // { name: "Bash", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bash/bash-original.svg" },
-  // { name: "Vim", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vim/vim-original.svg" },
    { name: "VS Code", link: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg" },
-  // {name: "", link: "https://repository-images.githubusercontent.com/59065830/b62be480-45d2-11ea-9989-803db0f9c44d"}
 ]
 
 export const contactLinks = [
@@ -35,21 +32,4 @@ export const serviceData = [
     desc: 'Web design encompasses many different skills and disciplines in the production and maintenance of websites. The different areas of web design include web graphic design user interface design  authoring, including standardised code and proprietary software user experience design and search engine',
     img: "https://img.icons8.com/external-kiranshastry-lineal-color-kiranshastry/64/000000/external-web-development-coding-kiranshastry-lineal-color-kiranshastry.png"
   },
-  // {
-  //   name: 'Mobile Application Development',
-  //   desc: `We provide a range of mobile application development services
-  //   including custom mobile development on Android platforms, building
-  //   cross-platform apps, designing user experience and integrating
-  //   novel mobile interfaces such as chat and voice`,
-  //   img: "https://img.icons8.com/external-justicon-lineal-color-justicon/64/000000/external-app-development-responsive-web-design-justicon-lineal-color-justicon.png"
-  // },
-  // {
-  //   name: 'Backend Development',
-  //   desc: `Graphic design is a craft where professionals create visual
-  //   content to communicate messages. By applying visual hierarchy and
-  //   page layout techniques, designers use typography and pictures to
-  //   meet users' specific needs and focus on the logic of displaying
-  //   elements in interactive designs, to optimize the user experience.`,
-  //   img: "https://img.icons8.com/external-flaticons-flat-flat-icons/64/000000/external-backend-no-code-flaticons-flat-flat-icons.png"
-  // },
 ]
